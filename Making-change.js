fc0e4cbd3205346e34fa4db6e6e9360b385ac0ca
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-04-09 19:08:06 -0700

    Update Making-change.js

--- Making-change.js
+++ Making-change.js
@@ -1,13 +1,16 @@
 /*
-Complete the method that will determine the minimum number of coins needed to make change for a given amount in American currency.
+Complete the method that will determine the minimum number of coins 
+needed to make change for a given amount in American currency.
 
 Coins used will be half-dollars, quarters, dimes, nickels, and pennies, 
 worth 50¢, 25¢, 10¢, 5¢ and 1¢, respectively. 
-They'll be represented by the symbols H, Q, D, N and P (symbols in Ruby, strings in in other languages)
+They'll be represented by the symbols H, Q, D, N and P.
 
 The argument passed in will be an integer representing the value in cents. 
 The return value should be a hash/dictionary/object with the symbols as keys, 
-and the numbers of coins as values. Coins that are not used should not be included in the hash. 
+and the numbers of coins as values.
+
+Coins that are not used should not be included in the hash. 
 If the argument passed in is 0, then the method should return an empty hash.
 
 Examples:
